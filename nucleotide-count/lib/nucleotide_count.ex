ce38be99c6b6dd3574a906eed076c011241b3d94
Eduardo Díaz <ediaz@dcc8090.cl>
2021-08-10 11:57:55 -0400

    improves nucleotide count

--- nucleotide-count/lib/nucleotide_count.ex
+++ nucleotide-count/lib/nucleotide_count.ex
@@ -15,8 +15,7 @@ defmodule NucleotideCount do
   @spec count(charlist(), char()) :: non_neg_integer()
   def count(strand, nucleotide) do
     strand
-    |> Enum.filter(&(&1 == nucleotide))
-    |> Enum.count()
+    |> Enum.count(&(&1 == nucleotide))
   end
 
   @doc """
@@ -30,7 +29,6 @@ defmodule NucleotideCount do
   @spec histogram(charlist()) :: map()
   def histogram(strand) do
     @nucleotides
-    |> Enum.map(&{&1, count(strand, &1)})
-    |> Map.new()
+    |> Enum.into(%{}, &{&1, count(strand, &1)})
   end
 end
