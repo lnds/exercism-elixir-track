d0694fb66c504dd45074be01a13e24d5b85182cf
Eduardo Díaz <ediaz@dcc8090.cl>
2021-09-27 13:38:42 -0300

    solve log level properly

--- log-level/lib/log_level.ex
+++ log-level/lib/log_level.ex
@@ -1,23 +1,24 @@
 defmodule LogLevel do
   def to_label(level, legacy?) do
-    case level do
-      0 when not legacy? -> :trace
-      1 -> :debug
-      2 -> :info
-      3 -> :warning
-      4 -> :error
-      5 when not legacy? -> :fatal
-      _ -> :unknown
+    cond do
+      level == 0 && not legacy? -> :trace
+      level == 1 -> :debug
+      level == 2 -> :info
+      level == 3 -> :warning
+      level == 4 -> :error
+      level == 5 && not legacy? -> :fatal
+      true -> :unknown
     end
   end
 
   def alert_recipient(level, legacy?) do
-    case to_label(level, legacy?) do
-      :error -> :ops
-      :fatal -> :ops
-      :unknown when legacy? -> :dev1
-      :unknown -> :dev2
-      _ -> false
+    label = to_label(level, legacy?)
+
+    cond do
+      label == :error || label == :fatal -> :ops
+      label == :unknown && legacy? -> :dev1
+      label == :unknown -> :dev2
+      true -> false
     end
   end
 end
