ad830f72fdb27761f4a2ac5b405b07a90c30980f
Eduardo Díaz <ediaz@dcc8090.cl>
2021-09-28 10:18:06 -0300

    solve resistor color duo

---
+++ resistor-color-duo/lib/resistor_color_duo.ex
@@ -0,0 +1,22 @@
+defmodule ResistorColorDuo do
+  @map %{
+    black: 0,
+    brown: 1,
+    red: 2,
+    orange: 3,
+    yellow: 4,
+    green: 5,
+    blue: 6,
+    violet: 7,
+    grey: 8,
+    white: 9
+  }
+
+  @doc """
+  Calculate a resistance value from two colors
+  """
+  @spec value(colors :: [atom]) :: integer
+  def value([first, second | _]) do
+    @map[first] * 10 + @map[second]
+  end
+end
