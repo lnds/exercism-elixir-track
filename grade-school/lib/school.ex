a94b97d154e42e142093976d58e397f09dc0b08b
Eduardo Díaz <ediaz@dcc8090.cl>
2021-12-17 18:12:29 -0300

    soled grade school

---
+++ grade-school/lib/school.ex
@@ -0,0 +1,44 @@
+defmodule School do
+  @moduledoc """
+  Simulate students in a school.
+
+  Each student is in a grade.
+  """
+
+  @doc """
+  Add a student to a particular grade in school.
+  """
+  @spec add(map, String.t(), integer) :: map
+  def add(db, name, grade) do
+    grades = Map.keys(db)
+    students = Enum.map(grades, &db[&1]) |> Enum.concat()
+
+    if name in students do
+      db
+    else
+      Map.update(db, grade, [name], fn students ->
+        [name | students]
+      end)
+    end
+  end
+
+  @doc """
+  Return the names of the students in a particular grade.
+  """
+  @spec grade(map, integer) :: [String.t()]
+  def grade(db, grade) do
+    Map.get(db, grade, [])
+  end
+
+  @doc """
+  Sorts the school by grade and name.
+  """
+  @spec sort(map) :: [{integer, [String.t()]}]
+  def sort(db) do
+    db
+    |> Enum.map(fn {k, v} ->
+      {k, Enum.sort(v)}
+    end)
+    |> Enum.into([])
+  end
+end
