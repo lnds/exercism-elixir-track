eb7bb76996a00b03e4a67334440fa26d8167c504
Eduardo Díaz <ediaz@dcc8090.cl>
2025-02-14 18:54:33 -0300

    update exercise freelancer-rates

--- freelancer-rates/lib/freelancer_rates.ex
+++ freelancer-rates/lib/freelancer_rates.ex
@@ -1,20 +1,18 @@
 defmodule FreelancerRates do
-  @factor 8.0
   def daily_rate(hourly_rate) do
-    hourly_rate * @factor
+    hourly_rate * 8.0
   end
 
   def apply_discount(before_discount, discount) do
-    before_discount - before_discount * discount / 100.0
+    before_discount * (100-discount) / 100.0 
   end
 
-  @days_per_month 22
   def monthly_rate(hourly_rate, discount) do
-    (@days_per_month * daily_rate(hourly_rate)) |> apply_discount(discount) |> ceil()
+    apply_discount(22 * hourly_rate * 8, discount) |> ceil()
   end
 
   def days_in_budget(budget, hourly_rate, discount) do
     rate = daily_rate(hourly_rate) |> apply_discount(discount)
-    Float.floor(budget / rate, 1)
+    Float.floor(budget / rate, 1) 
   end
 end
