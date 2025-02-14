df83895a0de1204d724ce721f59dc5f9801adee6
Eduardo Díaz <ediaz@dcc8090.cl>
2025-02-14 19:16:20 -0300

    update exercise language-list

--- language-list/lib/language_list.ex
+++ language-list/lib/language_list.ex
@@ -1,28 +1,27 @@
 defmodule LanguageList do
   def new() do
-    []
+    [] 
   end
 
   def add(list, language) do
-    [language | list]
+    [language | list] 
   end
 
   def remove([]), do: []
-  def remove([h | t]), do: t
+  def remove([_]), do: []
+  def remove([_|t]), do: t 
 
   def first([]), do: []
-  def first([h | t]), do: h
+  def first([a]), do: a
+  def first([h|_]), do: h  # Please implement the first/1 function
 
   def count([]), do: 0
-  def count([_ | t]), do: 1 + count(t)
+  def count([_]), do: 1
+  def count([_|t]), do: 1 + count(t)
 
-  def exciting_list?([]), do: false
-
-  def exciting_list?([h | t]) do
-    if h == "Elixir" do
-      true
-    else
-      exciting_list?(t)
-    end
+  def functional_list?([]), do: false
+  def functional_list?([h|t]) do
+    if h == "Elixir", do: true, else: functional_list?(t)
   end
+
 end
