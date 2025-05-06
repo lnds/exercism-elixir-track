56796b1dbae779adcbf471779a5385029a5e02e6
Eduardo Díaz <ediaz@dcc8090.cl>
2025-05-05 20:02:54 -0400

    solve high-school-sweetheart

--- high-school-sweetheart/lib/high_school_sweetheart.ex
+++ high-school-sweetheart/lib/high_school_sweetheart.ex
@@ -1,34 +1,22 @@
 defmodule HighSchoolSweetheart do
   def first_letter(name) do
-    name |> String.trim() |> String.first()
+    name |> String.trim() |> String.at(0)
   end
 
   def initial(name) do
-    (String.capitalize(name) |> first_letter()) <> "."
+    name |> first_letter() |> String.upcase() |> Kernel.<>(".")
   end
 
   def initials(full_name) do
-    String.split(full_name) |> Enum.map(&initial/1) |> Enum.join(" ")
+    [first, last] = full_name |> String.split()
+    "#{initial(first)} #{initial(last)}"
   end
 
   def pair(full_name1, full_name2) do
-    res = ~s"""
-         ******       ******
-       **      **   **      **
-     **         ** **         **
-    **            *            **
-    **                         **
-    **     #{initials(full_name1)}  +  #{initials(full_name2)}     **
-     **                       **
-       **                   **
-         **               **
-           **           **
-             **       **
-               **   **
-                 ***
-                  *
     """
-
-    res
+    ❤-------------------❤
+    |  #{initials(full_name1)}  +  #{initials(full_name2)}  |
+    ❤-------------------❤
+    """
   end
 end
