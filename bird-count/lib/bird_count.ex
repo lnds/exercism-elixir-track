c567288e9a40052cf233736f9be1574ff964fab7
Eduardo Díaz <ediaz@dcc8090.cl>
2025-05-05 21:14:12 -0400

    solve bird-count

--- bird-count/lib/bird_count.ex
+++ bird-count/lib/bird_count.ex
@@ -1,25 +1,28 @@
 defmodule BirdCount do
   def today([]), do: nil
-
-  def today([td | _]), do: td
+  def today([head]), do: head
+  def today([head | _tail]), do: head
 
   def increment_day_count([]), do: [1]
-
-  def increment_day_count([h | t]), do: [h + 1 | t]
+  def increment_day_count([head]), do: [head + 1]
+  def increment_day_count([head | tail]), do: [head + 1 | tail]
 
   def has_day_without_birds?([]), do: false
-  def has_day_without_birds?([h | t]), do: h == 0 || has_day_without_birds?(t)
+  def has_day_without_birds?([n]), do: n == 0
 
-  def total([]), do: 0
-  def total([h | t]), do: h + total(t)
-
-  def busy_days([]), do: 0
-
-  def busy_days([h | t]) do
-    if h >= 5 do
-      1 + busy_days(t)
+  def has_day_without_birds?([head | tail]) do
+    if head == 0 do
+      true
     else
-      busy_days(t)
+      has_day_without_birds?(tail)
     end
   end
+
+  def total([]), do: 0
+  def total([n]), do: n
+  def total([head | tail]), do: head + total(tail)
+
+  def busy_days([]), do: 0
+  def busy_days([n]), do: if(n >= 5, do: 1, else: 0)
+  def busy_days([head | tail]), do: if(head >= 5, do: 1 + busy_days(tail), else: busy_days(tail))
 end
