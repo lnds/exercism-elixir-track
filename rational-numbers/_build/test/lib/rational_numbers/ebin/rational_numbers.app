171105fa087110749722ea9dcf5dfe58cda49f9f
Eduardo Díaz <ediaz@dcc8090.cl>
2025-05-05 20:50:21 -0400

    solve rational-numbers

--- rational-numbers/_build/test/lib/rational_numbers/ebin/rational_numbers.app
+++ rational-numbers/_build/test/lib/rational_numbers/ebin/rational_numbers.app
@@ -1,6 +1,7 @@
 {application,rational_numbers,
-             [{applications,[kernel,stdlib,elixir,logger]},
+             [{modules,['Elixir.RationalNumbers']},
+              {optional_applications,[]},
+              {applications,[kernel,stdlib,elixir,logger]},
               {description,"rational_numbers"},
-              {modules,['Elixir.RationalNumbers']},
               {registered,[]},
               {vsn,"0.1.0"}]}.
--- rational-numbers/lib/rational_numbers.ex
+++ rational-numbers/lib/rational_numbers.ex
@@ -6,7 +6,7 @@ defmodule RationalNumbers do
   """
   @spec add(a :: rational, b :: rational) :: rational
   def add({a1, b1}, {a2, b2}) do
-    reduce({a1 * b2 + a2 * b1, b1 * b2})
+    {a1 * b2 + a2 * b1, b1 * b2} |> reduce()
   end
 
   @doc """
@@ -14,7 +14,7 @@ defmodule RationalNumbers do
   """
   @spec subtract(a :: rational, b :: rational) :: rational
   def subtract({a1, b1}, {a2, b2}) do
-    reduce({a1 * b2 - a2 * b1, b1 * b2})
+    {a1 * b2 - a2 * b1, b1 * b2} |> reduce()
   end
 
   @doc """
@@ -22,7 +22,7 @@ defmodule RationalNumbers do
   """
   @spec multiply(a :: rational, b :: rational) :: rational
   def multiply({a1, b1}, {a2, b2}) do
-    reduce({a1 * a2, b1 * b2})
+    {a1 * a2, b1 * b2} |> reduce()
   end
 
   @doc """
@@ -30,27 +30,28 @@ defmodule RationalNumbers do
   """
   @spec divide_by(num :: rational, den :: rational) :: rational
   def divide_by({a1, b1}, {a2, b2}) do
-    reduce({a1 * b2, a2 * b1})
+    {a1 * b2, a2 * b1} |> reduce
   end
 
   @doc """
   Absolute value of a rational number
   """
   @spec abs(a :: rational) :: rational
-  def abs({a, b}) do
-    {Kernel.abs(a), Kernel.abs(b)}
+  def abs({p, q}) do
+    {Kernel.abs(p), Kernel.abs(q)} |> reduce()
   end
 
   @doc """
   Exponentiation of a rational number by an integer
   """
   @spec pow_rational(a :: rational, n :: integer) :: rational
-  def pow_rational({a, b}, n) do
-    if n >= 0 do
-      {Integer.pow(a, n), Integer.pow(b, n)}
-    else
-      {Integer.pow(b, -n), Integer.pow(a, -n)}
-    end
+  def pow_rational({a, b}, n) when is_integer(n) and n >= 0 do
+    {Integer.pow(a, n), Integer.pow(b, n)} |> reduce()
+  end
+
+  def pow_rational({a, b}, n) when is_integer(n) and n < 0 do
+    m = Kernel.abs(n)
+    {Integer.pow(b, m), Integer.pow(a, m)} |> reduce()
   end
 
   @doc """
@@ -58,26 +59,17 @@ defmodule RationalNumbers do
   """
   @spec pow_real(x :: integer, n :: rational) :: float
   def pow_real(x, {a, b}) do
-    cond do
-      a == 0 -> 1.0
-      a < 0 -> 1.0 / :math.pow(x, -a / b)
-      true -> :math.pow(x, a / b)
-    end
+    :math.pow(x, a / b)
   end
 
   @doc """
   Reduce a rational number to its lowest terms
   """
   @spec reduce(a :: rational) :: rational
-  def reduce({a1, a2}) do
-    g = Integer.gcd(a1, a2)
-    a = div(a1, g)
-    b = div(a2, g)
+  def reduce({a, b}) when b < 0, do: reduce({-a, -b})
 
-    if b < 0 do
-      {-a, -b}
-    else
-      {a, b}
-    end
+  def reduce({a, b}) do
+    g = Integer.gcd(a, b)
+    {a / g, b / g}
   end
 end
