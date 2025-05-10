08d9bc11c905e308ec20ca6dc2d5af84ebc28b48
Eduardo Díaz <ediaz@dcc8090.cl>
2025-05-10 09:36:58 -0400

    solve rpg-character-sheet

--- rpg-character-sheet/lib/rpg/character_sheet.ex
+++ rpg-character-sheet/lib/rpg/character_sheet.ex
@@ -1,18 +1,23 @@
 defmodule RPG.CharacterSheet do
   def welcome() do
+    # Please implement the welcome/0 function
     IO.puts("Welcome! Let's fill out your character sheet together.")
+    :ok
   end
 
   def ask_name() do
-    IO.gets("What is your character's name?\n") |> String.trim()
+    IO.gets("What is your character's name?\n") |> to_string() |> String.trim()
   end
 
   def ask_class() do
-    IO.gets("What is your character's class?\n") |> String.trim()
+    IO.gets("What is your character's class?\n") |> to_string() |> String.trim()
   end
 
   def ask_level() do
-    IO.gets("What is your character's level?\n") |> String.trim() |> String.to_integer()
+    IO.gets("What is your character's level?\n")
+    |> to_string()
+    |> String.trim()
+    |> String.to_integer()
   end
 
   def run() do
@@ -20,8 +25,6 @@ defmodule RPG.CharacterSheet do
     name = ask_name()
     class = ask_class()
     level = ask_level()
-
-    %{class: class, level: level, name: name}
-    |> IO.inspect(label: "\nYour character")
+    %{class: class, level: level, name: name} |> IO.inspect(label: "\nYour character")
   end
 end
