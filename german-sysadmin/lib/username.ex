11db07f3ed64c1b27938581f32ac124bd11d5306
Eduardo Díaz <ediaz@dcc8090.cl>
2025-05-10 09:16:52 -0400

    solve german-sysadmin

--- german-sysadmin/lib/username.ex
+++ german-sysadmin/lib/username.ex
@@ -3,11 +3,11 @@ defmodule Username do
 
   def sanitize([h | t]) do
     case h do
-      ?ä -> 'ae' ++ sanitize(t)
-      ?ö -> 'oe' ++ sanitize(t)
-      ?ü -> 'ue' ++ sanitize(t)
-      ?ß -> 'ss' ++ sanitize(t)
-      h when h in 'abcdefghijklmnopqrstuvwxyz_' -> [h | sanitize(t)]
+      ?ä -> ~c"ae" ++ sanitize(t)
+      ?ö -> ~c"oe" ++ sanitize(t)
+      ?ü -> ~c"ue" ++ sanitize(t)
+      ?ß -> ~c"ss" ++ sanitize(t)
+      h when h in ~c"abcdefghijklmnopqrstuvwxyz_" -> [h | sanitize(t)]
       _ -> sanitize(t)
     end
   end
